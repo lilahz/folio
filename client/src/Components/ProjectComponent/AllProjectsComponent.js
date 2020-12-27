3a8a8c7f09842e95b5037701336e671fa0b47aef
ayeletbiran <ayeletbiran3@gmail.com>
2020-12-27 09:50:23 +0200

    changes in filter projects

--- client/src/Components/ProjectComponent/AllProjectsComponent.js
+++ client/src/Components/ProjectComponent/AllProjectsComponent.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import {Row} from 'react-bootstrap';
 
 import ProjectComponent from './ProjectComponent';
-// import FilterProjectsComponent from './FilterProjectsComponent';
+import FilterProjectComponent from './FilterProjectsComponent';
 import './ProjectComponent.css';
 import './AllProjectsComponent.css';
 
@@ -63,10 +63,20 @@ class AllProjectComponent extends Component {
         else if (!isLoaded) {
             return <div>Loading...</div>}
         else { 
-            // const field_array = ["Marketing", "App Development", "Web Development",
-            //                     "Logo Design", "Web Design", "Financial Aid", "Legal Aid", "Sales"]
-            // const status_array = ["Not Started", "In Progress", "Completed"]
-            
+            const field_array = [{ value: 'marketing', label: 'Marketing' },
+                                 { value: 'appdev', label: 'App Development' },
+                                 { value: 'webdev', label: 'Web Development' },
+                                 { value: 'logodesign', label: 'Logo Design' },
+                                 { value: 'webdesign', label: 'Web Design' },
+                                 { value: 'logodesign', label: 'Logo Design' },
+                                 { value: 'finaid', label: 'Financial Aid' },
+                                 { value: 'legalaid', label: 'Legal Aid' },
+                                 { value: 'sales', label: 'Sales' }]
+
+            const status_array = [{value: 'todo', label: 'To Do'},
+                                    {value: 'inprogress', label: 'In Progress'},
+                                    {value: 'done', label: 'Done'}]
+
             const projects_array = (<Row style={RowStyle}>
                 {items.map((project) => (
                     <ProjectComponent className="col-sm-4"
@@ -76,12 +86,21 @@ class AllProjectComponent extends Component {
                 </Row>)
             return <div className="ProjectsList" style={this.state}>
                         <h1 class="ProjectListHeader">All of Our Projects</h1>
-                        {/* <FilterProjectsComponent className="FilterComp"
-                                                 optionArray={field_array}
-                                                 filterName="fieldFilter"/>
-                        <FilterProjectsComponent className="FilterComp"
-                                                optionArray={status_array}
-                                                filterName="statusFilter"/> */}
+                        <div class="container">
+                            <div className="row justify-content-center" style={{margin: '30px', }}>
+                                <div className="col-4 text-center" 
+                                    style={{marginRight: '30px'}}>
+                                    <FilterProjectComponent
+                                                        options={field_array}
+                                                        filterName="Filter by Field"/>
+                                </div>
+                                <div className="col-4 text-center" style={{marginLeft: '30px'}}>
+                                    <FilterProjectComponent 
+                                                        options={status_array}
+                                                        filterName="Filter by Status"/>
+                                </div>
+                            </div>
+                        </div>
                         {projects_array}
                     </div>
         }            
--- client/src/Components/ProjectComponent/FilterProjectsComponent.js
+++ client/src/Components/ProjectComponent/FilterProjectsComponent.js
@@ -1,13 +1,90 @@
-// import React from 'react';
-// import DropdownMultiselect from "react-multiselect-dropdown-bootstrap";
+import React, { Component } from 'react'
+import Select from 'react-select'
+import makeAnimated from 'react-select/animated';
+import axios from 'axios'
 
-// // import './FilterProjectComponent.css';
+// import './FilterProjectComponent.css';
 
-// const FilterProjectsComponent = props => {
-//     const optionArray = props.optionArray   
-//     const filterName = props.filterName
+class FilterProjectComponent extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            selectOptions : props.options,
+            placeholder : props.filterName,
+            value: []
+        }
+    }
 
-//     return <DropdownMultiselect class="buttonClass" options={optionArray} name={filterName} />;
-// }
+    animatedComponents = makeAnimated();
 
-// export default FilterProjectsComponent;
+    customStyles = {
+        option: (provided, state) => ({
+        ...provided,
+        borderBottom: '1px dotted blue',
+        padding: 10,
+        width: 350,
+        fontSize: 15
+        }),
+        control: styles => ({
+            ...styles, backgroundColor: 'white', width: 350 }),
+        singleValue: (provided, state) => {
+        const opacity = state.isDisabled ? 0.5 : 1;
+        const transition = 'opacity 300ms';
+        return { ...provided, opacity, transition };
+        },
+        menu: styles => ({
+            ...styles,
+            width: 350,
+            backgroundColor: 'white'
+        }),
+    }
+
+    handleChange (e) {
+        this.setState({value:e})
+    }
+
+    axios = require('axios');
+    async updateData() {
+        params = {
+            filter_array: this.state.value
+          }
+    
+        let res = await axios.post('/home/projects', params);
+        console.log(res.data);
+    }
+    // updateData () {
+    //     async function updatePost() {
+    //         const requestOptions = {
+    //             method: 'PUT',
+    //             headers: { 'Content-Type': 'application/json' },
+    //             body: JSON.stringify({ filter_array: this.state.value })
+    //         };
+    //         const response = await fetch('/home/projects', requestOptions);
+    //         const data = await response.json();
+    //     }
+    // }
+
+    
+    render() {
+        console.log(this.state.selectOptions)
+        return (<div><Select className="mt-4 col-md-8 col-offset-4 col-sm-4"
+            maxMenuHeight={170}
+            hideSelectedOptions={false}
+            styles={this.customStyles}
+            closeMenuOnSelect={false}
+            components={this.animatedComponents}
+            isMulti
+            options={this.state.selectOptions}
+            placeholder={this.state.placeholder}
+            onChange={this.handleChange.bind(this)}
+            />
+            {
+                this.state.value === null ? "" :
+                this.updateData()
+            }
+            </div>
+        )
+    }
+}
+
+export default FilterProjectComponent;

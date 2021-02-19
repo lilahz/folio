9f1e801ebd821a835c8f909a94ce7e3943927c01
lilahz <lilahz05@gmail.com>
2021-02-19 12:43:44 +0200

    trying to upload flask to heroku

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+import os
+
 from server import create_app
 
-
-app = create_app('dev')
+is_prod = os.environ.get('IS_HEROKU', None)
+if is_prod:
+    app = create_app('prod')
+else:
+    app = create_app('dev')
 
 if __name__ == '__main__':
     app.run()
